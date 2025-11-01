ed35ecfd2acecd1a232b37735351af8bf5ca0cda
Lucciano Máximo <luccianobrzl@gmail.com>
2025-11-01 17:56:17 -0500

    Verificando cambios

--- app/vistas/inicioVista.php
+++ app/vistas/inicioVista.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <title> - Mecánica Automotriz</title>
+    <title>Xtreme Performance - Mecánica Automotriz</title>
     <meta name="title" content="Xtreme Performance - Mecánica Automotriz" />
     <meta
       name="description"
