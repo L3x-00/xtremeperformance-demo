f79689f012522b08a5ab352a8849275d634c02f8
Lucciano Máximo <luccianobrzl@gmail.com>
2025-11-02 21:13:02 -0500

    arreglando modificaciond de imagenes

--- app/vistas/seguimientosAltaVista.php
+++ app/vistas/seguimientosAltaVista.php
@@ -13,8 +13,37 @@
 
   <div class="form-group text-left">
       <label for="fotos">Foto:</label>
-      <input type="file" name="fotos[]" id="fotos" multiple class="form-control" value="<?php print isset($datos['data']['fotos'])?$datos['data']['fotos']:''; ?>" <?php if (isset($datos["baja"])) { print " disabled "; }?>>
+      <input type="file" name="fotos[]" id="fotos" multiple class="form-control" <?php if (isset($datos["baja"])) { print " disabled "; }?>>
+      <small class="form-text text-muted">Por seguridad del navegador, las imágenes existentes no pueden preseleccionarse en este campo. Puedes subir imágenes nuevas o borrar/visualizar las actuales abajo.</small>
     </div>
+
+  <?php if (!isset($datos["baja"]) && isset($datos['data']['id']) && isset($datos['idOrdenReparacion'])) { 
+    $carpeta = "fotos/".$datos['idOrdenReparacion']."/".$datos['data']['id']."/"; 
+    if (file_exists($carpeta)) { 
+      $archivos = scandir($carpeta); 
+      if (count($archivos) > 2) { ?>
+        <div class="mt-3">
+          <label class="form-label">Imágenes actuales:</label>
+          <div class="d-flex flex-wrap gap-2 align-items-start">
+            <?php for ($i=2; $i<count($archivos); $i++) { 
+              $src = RUTA."public/".$carpeta.$archivos[$i]; ?>
+              <div class="text-center me-2 mb-2">
+                <a href="<?php print RUTA.'seguimientos/mostrarImagen/'.$datos['data']['id'].'/'.$i; ?>" target="_blank">
+                  <img src="<?php print $src; ?>" alt="Imagen <?php print $i; ?>" class="border rounded" style="height:60px; width:auto;">
+                </a>
+                <div>
+                  <a class="btn btn-sm btn-outline-danger mt-1" href="<?php print RUTA.'seguimientos/borrarImagen/'.$datos['data']['id'].'/'.$i; ?>" onclick="return confirm('¿Borrar imagen?');">Borrar</a>
+                </div>
+              </div>
+            <?php } ?>
+          </div>
+          <div class="mt-2">
+            <a class="btn btn-secondary btn-sm" href="<?php print RUTA.'seguimientos/desplegarSeguimiento/'.$datos['data']['id'].'/'.$datos['pagina']; ?>">Ver todas</a>
+          </div>
+        </div>
+      <?php } 
+    } 
+  } ?>
 
  
     <div class="form-group text-start mb-3">
