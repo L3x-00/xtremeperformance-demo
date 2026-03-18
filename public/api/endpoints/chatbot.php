cadb12fa3d13779c8b39bef77da23adf7a5715ea
Lucciano Máximo <luccianobrzl@gmail.com>
2026-03-18 11:28:47 -0500

    Validando funcionalidades del chatbot

--- public/api/endpoints/chatbot.php
+++ public/api/endpoints/chatbot.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-// 2. Si el navegador pregunta por permisos (OPTIONS), le decimos que SÍ y salimos
+
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
     exit();
