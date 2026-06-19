c9de2e5c8c455b6c62fad4aa30208d73c9ecad8a
Lucciano Máximo Vivas Barzola <luccianobrzl@gmail.com>
2026-06-18 21:16:47 -0500

    Refactor index.php to handle URL parsing

--- public/index.php
+++ public/index.php
@@ -4,6 +4,10 @@ header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
 header('Cache-Control: post-check=0, pre-check=0', false);
 header('Pragma: no-cache');
 header('Expires: 0');
+if (!isset($_GET['url']) && isset($_SERVER['REQUEST_URI'])) {
+    $ruta = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $_GET['url'] = ltrim($ruta, '/');
+}
 require_once("../app/inicio.php");
 $control = new Control();
 ?>
